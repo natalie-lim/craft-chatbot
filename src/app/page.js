67593d9b954b72e22b70e58627faa33e2f2adc81
Natalie Lim <natalielim1906@gmail.com>
2025-06-05 01:27:12 -0700

    added typing feature for frontend

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Space_Grotesk } from "next/font/google";
 import { signInWithPopup } from "firebase/auth";
 import {setDoc, doc, getDoc} from "firebase/firestore";
@@ -11,6 +11,25 @@ const spaceGrotesk = Space_Grotesk({
   subsets: ['latin'],
   weight: ['400', '600'],
 });
+
+function TypingText({ text, delay = 100 }) {
+  const [displayedText, setDisplayedText] = useState('');
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+      if (index < text.length) {
+          const intervalId = setInterval(() => {
+              setDisplayedText(prevText => prevText + text[index]);
+              setIndex(prevIndex => prevIndex + 1);
+          }, delay);
+
+          return () => clearInterval(intervalId);
+      }
+  }, [displayedText, index, text, delay]);
+
+  return <span className={`${spaceGrotesk.className} text-5xl font-semibold pt-4`}>{displayedText}</span>;
+}
+
 
 async function checkIfUserExists({docName}) {
   const docRef = doc(db, "userInfo", docName);
@@ -73,7 +92,7 @@ export default function Home() {
   
   return (
     <div className={`flex flex-col items-center min-h-screen justify-center transition-opacity duration-1000 ${fadeOut ? 'opacity-0' : 'opacity-100'}`}>
-      <h1 className={`${spaceGrotesk.className} text-6xl`}>task manager.</h1>
+      <TypingText delay = {150} text ="Task Manager." ></TypingText>
       <h2 className={`${spaceGrotesk.className} text-lg pt-4`}>let's get your life together</h2>
       <button 
         onClick={handleClick}
