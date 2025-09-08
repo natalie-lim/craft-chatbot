f97d2ee4cc66bc6be4ff788b97fba900465e7993
Natalie Lim <natalielim1906@gmail.com>
2025-09-08 10:21:15 -0400

    sewing colors refined

--- src/app/sewingChatbot/page.jsx
+++ src/app/sewingChatbot/page.jsx
@@ -7,15 +7,24 @@ import TypingText from "../TypingText";
 import { Scissors, Pin, Spool, User } from "lucide-react";
 import Chatbot from "../Chatbot";
 
-const spaceGrotesk = Space_Grotesk({ subsets: ["latin"], weight: ["400", "600"] });
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["400", "600"],
+});
 
 export default function SewingChatbot() {
   const router = useRouter();
   const go = (path) => () => router.push(path);
 
   return (
-    <div className={`${spaceGrotesk.className} bg-[#f0efeb] min-h-screen w-full`}>
-        <Chatbot category="Sewing" textColor="text-[#d4afb9]" bgColor="bg-[#d4afb9]"/> 
+    <div
+      className={`${spaceGrotesk.className} bg-[#f0efeb] min-h-screen w-full`}
+    >
+      <Chatbot
+        category="Sewing"
+        textColor="text-[#d4afb9]"
+        bgColor="bg-[#F0DAE0]"
+      />
     </div>
   );
 }
